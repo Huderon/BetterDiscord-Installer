d6e037133e0f2d034b3e8820fd8ee114d6cf0c2c
Zerebos <me@zerebos.com>
2026-07-15 01:17:42 -0400

    force wails compat with goreleaser

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.26.0
 require (
 	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c
 	github.com/shirou/gopsutil/v3 v3.24.2
+	github.com/tc-hib/winres v0.3.1
 	github.com/wailsapp/wails/v2 v2.11.0
 )
 
@@ -24,6 +25,7 @@ require (
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
@@ -38,6 +40,7 @@ require (
 	github.com/wailsapp/mimetype v1.4.1 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	golang.org/x/crypto v0.33.0 // indirect
+	golang.org/x/image v0.12.0 // indirect
 	golang.org/x/net v0.35.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
---
+++ scripts/winres/main.go
@@ -0,0 +1,102 @@
+// Generates the Windows resource object (.syso) that `go build` links into
+// the executable: application icon, DPI/common-controls manifest, and version
+// info. This mirrors what `wails build` does internally (see wails'
+// pkg/commands/build/packager.go) using the same assets in build/windows/,
+// since GoReleaser invokes plain `go build` which does not generate one.
+//
+// Invoked by the GoReleaser pre-build hook for the Windows build:
+//
+//	go run ./scripts/winres -version <semver>
+//
+// The output filename carries the _windows_amd64 suffix so Go's implicit
+// build constraints keep it out of builds for other targets.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+
+	"github.com/tc-hib/winres"
+	"github.com/tc-hib/winres/version"
+)
+
+const (
+	name        = "BetterDiscord Installer"
+	company     = "BetterDiscord"
+	copyright   = "© BetterDiscord"
+	description = "A simple standalone program to install, repair, and uninstall BetterDiscord."
+)
+
+func main() {
+	ver := flag.String("version", "0.0.0", "release version (without v prefix)")
+	out := flag.String("out", "rsrc_windows_amd64.syso", "output file")
+	flag.Parse()
+
+	if err := run(*ver, *out); err != nil {
+		fmt.Fprintln(os.Stderr, "winres:", err)
+		os.Exit(1)
+	}
+}
+
+func run(ver, out string) error {
+	// Assembly identity and fixed file versions must be numeric (x.y.z),
+	// so strip any prerelease suffix like "-alpha.1".
+	numeric := "0.0.0"
+	if m := regexp.MustCompile(`^\d+\.\d+\.\d+`).FindString(ver); m != "" {
+		numeric = m
+	}
+
+	rs := winres.ResourceSet{}
+
+	iconFile, err := os.Open("build/windows/icon.ico")
+	if err != nil {
+		return err
+	}
+	defer iconFile.Close()
+	ico, err := winres.LoadICO(iconFile)
+	if err != nil {
+		return fmt.Errorf("couldn't load icon from icon.ico: %w", err)
+	}
+	if err := rs.SetIcon(winres.RT_ICON, ico); err != nil {
+		return err
+	}
+
+	replacer := strings.NewReplacer(
+		"{{.Name}}", name,
+		"{{.Info.ProductVersion}}", numeric,
+		"{{.Info.CompanyName}}", company,
+		"{{.Info.ProductName}}", name,
+		"{{.Info.Copyright}}", copyright,
+		"{{.Info.Comments}}", description,
+	)
+
+	manifestData, err := os.ReadFile("build/windows/wails.exe.manifest")
+	if err != nil {
+		return err
+	}
+	manifest, err := winres.AppManifestFromXML([]byte(replacer.Replace(string(manifestData))))
+	if err != nil {
+		return err
+	}
+	rs.SetManifest(manifest)
+
+	versionData, err := os.ReadFile("build/windows/info.json")
+	if err != nil {
+		return err
+	}
+	var vi version.Info
+	if err := vi.UnmarshalJSON([]byte(replacer.Replace(string(versionData)))); err != nil {
+		return err
+	}
+	rs.SetVersionInfo(vi)
+
+	fout, err := os.Create(out)
+	if err != nil {
+		return err
+	}
+	defer fout.Close()
+	return rs.WriteObject(fout, winres.ArchAMD64)
+}
